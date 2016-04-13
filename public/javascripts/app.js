f6e720d85705cdbb94c4ff14f04c1ba84d520649
chinmaydhamankar <chinmaydhamankar1@gmail.com>
2016-04-13 09:03:19 +0530

    Resolved conflicts

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,30 @@
 /**
  * Created by pratiksanglikar on 12/04/16.
  */
+/*define(["angularAMD", "angular-route"], function (angularAMD) {
+	var app = angular.module("amazonfresh", ["ngRoute"]);
+	/!*app.config(function ($routeProvider) {
+		/!*$routeProvider.when("/home", angularAMD.route({
+			templateUrl: "views/home.html", controller: "MainControl",
+			controllerUrl: "controllers/MainControl"
+		}));*!/
+	});*!/
+	app.controller("MainControl",[ "$scope", function ($scope) {
+		$scope.message = "Hello World!";
+	}]);
+	return angularAMD.bootstrap(app);
+});*/
+
+define(['angularAMD', 'angular-route'], function (angularAMD) {
+	var app = angular.module("amazonfresh", ['ngRoute']);
+
+	app.config(function ($routeProvider) {
+		$routeProvider
+			.when("/index", angularAMD.route({
+				templateUrl: '/partials/view_home.html', controller: 'MainControl', controllerUrl: './controllers/MainControl'
+			}))
+			.otherwise({redirectTo: "/index"});
+	});
+
+	return angularAMD.bootstrap(app);
+});
