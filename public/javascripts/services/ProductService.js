9913c2ecec0951a5ab7072a0feac18d4990bbfbf
Pratik Sanglikar <pratik.sanglikar@sjsu.edu>
2016-04-24 18:41:20 -0700

    integrated products list page

---
+++ public/javascripts/services/ProductService.js
@@ -0,0 +1,9 @@
+/**
+ * Created by pratiksanglikar on 24/04/16.
+ */
+angular.module("amazonfresh").factory("ProductService", ["$http","$q", function ($http, $q) {
+	var ProductService = {
+		
+	};
+	return ProductService;
+}]);
