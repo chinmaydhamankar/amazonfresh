fa6d7e6454c0242a72ecfd1bca2e5bd518c6c9ab
shaileshyedge <yedgeshailesh@gmail.com>
2016-04-24 18:02:57 -0700

    Adding services for getting unapproved customers and products and approving them

--- javascripts/admin/adminhandler.js
+++ javascripts/admin/adminhandler.js
@@ -8,7 +8,6 @@ var PasswordManager = require("../authentication/passwordmanager");
 var Q = require("q");
 var UserTypes = require("../commons/constants").usertypes;
 
-
 exports.approvecreatefarmer = function (info) {
     console.log("come to approve");
     var deferred = Q.defer();
@@ -20,6 +19,98 @@ exports.approvecreatefarmer = function (info) {
         }).catch(function (error) {
             deferred.reject(error);
         });
+    return deferred.promise;
+};
+
+
+
+exports.approveproduct = function (info) {
+    var deferred = Q.defer();
+    var searchQuery = JSON.parse(info);
+    console.log(searchQuery);
+    console.log(searchQuery.info.productName);
+    var cursor = MongoDB.collection("products").update({"productName" : searchQuery.info.productName},{$set : { "isApproved" : true }});
+    cursor.then(function (user) {
+        deferred.resolve(user);
+    }).catch(function (error) {
+        deferred.reject(error);
+    });
+    return deferred.promise;
+};
+
+
+exports.approvecustomer = function (info) {
+    var deferred = Q.defer();
+    var searchQuery = JSON.parse(info);
+    console.log(searchQuery);
+    var cursor = MongoDB.collection("users").update({"ssn" : searchQuery.info.ssn},{$set : { "isApproved" : true }});
+    cursor.then(function (user) {
+        deferred.resolve(user);
+    }).catch(function (error) {
+        deferred.reject(error);
+    });
+    return deferred.promise;
+};
+
+
+
+
+exports.getAllUnApprovedFarmers = function() {
+    console.log("here again why");
+    var deferred = Q.defer();
+    var cursor = MongoDB.collection("users").find({"usertype" : "FARMER", "isApproved": false });
+    var farmerList = [];
+    cursor.each(function (err, doc) {
+        if (err) {
+            deferred.reject(err);
+        }
+        if (doc != null) {
+            farmerList.push(doc);
+            console.log(farmerList);
+        } else
+        {
+            deferred.resolve(farmerList);
+        }
+    })
+    return deferred.promise;
+};
+
+
+exports.getAllUnApprovedProducts = function() {
+    var deferred = Q.defer();
+    var cursor = MongoDB.collection("products").find({ "isApproved": false });
+    var productList = [];
+    cursor.each(function (err, doc) {
+        if (err) {
+            deferred.reject(err);
+        }
+        if (doc != null) {
+            productList.push(doc);
+            console.log(productList);
+        } else
+        {
+            deferred.resolve(productList);
+        }
+    })
+    return deferred.promise;
+};
+
+exports.getAllUnApprovedCustomers = function() {
+    var deferred = Q.defer();
+    var cursor = MongoDB.collection("users").find({"usertype" : "CUSTOMER", "isApproved": false});
+    var customerList = [];
+    cursor.each(function (err, doc) {
+        if (err) {
+            deferred.reject(err);
+        }
+        if (doc != null) {
+            customerList.push(doc);
+            console.log(customerList);
+        } else
+        {
+            deferred.resolve(customerList);
+        }
+    })
     return deferred.promise;
 };
 
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,6 @@ var router = express.Router();
 var adminHandler = require("../javascripts/admin/adminhandler");
 
 router.post("/", function (req, res) {
-    console.log("entered to approve");
     req.body.isApproved = true;
     var data = req.body;
     data = JSON.stringify(data);
@@ -24,6 +23,110 @@ router.post("/", function (req, res) {
             });
     });
 });
+
+
+
+
+
+router.get("/listunapprovedfarmers", function (req, res) {
+    console.log("here came");
+    var promise = adminHandler.getAllUnApprovedFarmers();
+    promise.done(function (unapprovedfarmers) {
+        res.send({
+            success: true,
+            error: null,
+            data: unapprovedfarmers
+        });
+    }, function (error) {
+        res.status(500)
+            .send({
+                success: false,
+                error: error,
+                data: null
+            });
+    });
+});
+
+
+router.get("/listunapprovedproducts", function (req, res) {
+    var promise = adminHandler.getAllUnApprovedProducts();
+    promise.done(function (unapprovedproducts) {
+        res.send({
+            success: true,
+            error: null,
+            data: unapprovedproducts
+        });
+    }, function (error) {
+        res.status(500)
+            .send({
+                success: false,
+                error: error,
+                data: null
+            });
+    });
+});
+
+
+router.get("/listunapprovedcustomers", function (req, res) {
+    var promise = adminHandler.getAllUnApprovedCustomers();
+    promise.done(function (unapprovedcustomers) {
+        res.send({
+            success: true,
+            error: null,
+            data: unapprovedcustomers
+        });
+    }, function (error) {
+        res.status(500)
+            .send({
+                success: false,
+                error: error,
+                data: null
+            });
+    });
+});
+
+router.post("/approveproduct", function (req, res) {
+    var data = req.body;
+    data = JSON.stringify(data);
+    var promise = adminHandler.approveproduct(data);
+    promise.done(function () {
+        res.send({
+            success: true,
+            error: null,
+            data: "Product has been approved successfully!"
+        });
+    }, function (error) {
+        res.status(500)
+            .send({
+                success: false,
+                error: error,
+                data: null
+            });
+    });
+});
+
+
+router.post("/approvecustomer", function (req, res) {
+    var data = req.body;
+    data = JSON.stringify(data);
+    var promise = adminHandler.approvecustomer(data);
+    promise.done(function () {
+        res.send({
+            success: true,
+            error: null,
+            data: "Customer has been approved successfully!"
+        });
+    }, function (error) {
+        res.status(500)
+            .send({
+                success: false,
+                error: error,
+                data: null
+            });
+    });
+});
+
+
 
 
 module.exports = router;
