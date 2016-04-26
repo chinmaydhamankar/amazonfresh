84c2cad36aa799d681843e1aa07aab10106c8be3
Pratik Sanglikar <pratik.sanglikar@sjsu.edu>
2016-04-26 03:43:54 -0700

    added static content of trip details to the right pane.

--- public/javascripts/controllers/TripsController.js
+++ public/javascripts/controllers/TripsController.js
@@ -5,4 +5,46 @@ angular.module("amazonfresh").controller("TripsController",["$scope","$routePara
 	function ($scope,$routeParams, TripsService) {
 		$scope.param = $routeParams.tripId;
 		$scope.tripID = "01d03e5c2dd78e2477e332c0794ee442df3de202";
-}]);
+
+		init = function () {
+			var promise = TripsService.getTripDetails($scope.tripID);
+			promise.then(function (tripDetails) {
+				$scope.tripDetails = tripDetails;
+				calculateDeliveryStatus(tripDetails);
+			}).catch(function (error) {
+				alert("Error: " + error);
+			});
+		}
+
+		calculateDeliveryStatus = function (tripDetails) {
+			$scope.orderDateString = calculateDateString(tripDetails.orderTime);
+			$scope.expectedDeliveryDateString = calculateDateString(tripDetails.deliveryTime);
+			$scope.deliveryStatus = getDeliveryStatus(tripDetails.deliveryTime);
+		}
+
+		calculateDateString = function (date) {
+			var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+			var string = "";
+			if(isNaN(date)) {
+				date = Number(date);
+			}
+			date = new Date(date);
+			string += date.getUTCDate() + " " + months[date.getUTCMonth()] + ", " + date.getUTCFullYear() + " ";
+			string += date.getUTCHours() + ":" + date.getUTCMinutes();
+			return string;
+		}
+
+		getDeliveryStatus = function (date) {
+			if(isNaN(date)) {
+				date = Number(date);
+			}
+			if(date >= new Date().getTime()) {
+				return "In Transit";
+			} else {
+				return "Delivered!";
+			}
+		}
+
+		init();
+	}
+]);
--- public/javascripts/services/TripsService.js
+++ public/javascripts/services/TripsService.js
@@ -2,6 +2,24 @@
  * Created by pratiksanglikar on 25/04/16.
  */
 angular.module("amazonfresh").factory("TripsService",["$http", "$q", function ($http, $q) {
-	var TripsService = {};
+	var TripsService = {
+		getTripDetails: function (tripID) {
+			var deferred = $q.defer();
+			var url = "http://localhost:3000/trips/id/" + tripID;
+			$http({
+				url: url,
+				method: "GET"
+			}).then(function (data) {
+				if(data.data.success) {
+					deferred.resolve(data.data.data);
+				} else {
+					deferred.reject(data.data.error);
+				}
+			}).catch(function (error) {
+				deferred.reject(error);
+			});
+			return deferred.promise;
+		}
+	};
 	return TripsService;
 }]);
